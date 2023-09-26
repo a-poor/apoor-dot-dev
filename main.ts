170b4ffc44fa49e6ca9872f97b005a69c27606b5
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-09-25 20:57:13 -0700

    Adds basic web server with links

--- main.ts
+++ main.ts
@@ -1,18 +1,38 @@
+import { Hono } from "https://deno.land/x/hono@v3.7.2/mod.ts";
+import { logger } from "https://deno.land/x/hono@v3.7.2/middleware.ts";
+
+import { kvData } from "./data.ts";
 
 
-function createHandler(): Deno.ServeHandler {
-  return (req, res) => {
-    const url = new URL(req.url);
-    console.log(url);
-    return new Response("Hello, World!");
-  };
-}
+const app = new Hono();
+app.use("*", logger());
 
+app.get("/", (c) => c.text("Hello Deno!"));
+app.get("/admin/*", (c) => c.notFound());
+app.get("/:key", (c) => {
+  // Get the key param...
+  const key = c.req.param("key").toLowerCase();
+
+  // Is key in kvData?
+  if (!(key in kvData)) {
+    return c.notFound();
+  }
+
+  // Get the data for the key...
+  const data = kvData[key];
+
+  // Get the URL...
+  const url = new URL(c.req.url);
+
+  // If the link is internal, add the UTM source...
+  if (data.inner) {
+    url.searchParams.set("utm_source", "austinpoor.com");
+  }
+
+  return c.redirect(url.toString(), 302);
+});
 
 // Run the server if this file is run as a script...
 if (import.meta.main) {
-  Deno.serve(
-    {},
-    createHandler(),
-  );
+  Deno.serve(app.fetch);
 }
